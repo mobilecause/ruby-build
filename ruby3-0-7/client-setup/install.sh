eb4dcc41a95af3835664300a2050661f5207ad8b
Santhosh Ramaiah <santhosh.r411@gmail.com>
2025-07-25 23:47:14 +0530

    simplify things

--- ruby3-0-7/client-setup/install.sh
+++ ruby3-0-7/client-setup/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-REPO_URL="https://raw.githubusercontent.com/santhoshramaiah/ruby-build/main"
+REPO_URL="https://raw.githubusercontent.com/USERNAME/REPOSITORY/main"
 
 echo "Installing Ruby 3.0.7 repository..."
 
 # Download repo config with priority=70
-curl -fsSL "$REPO_URL/ruby3-0-7/client-setup/ruby-build.repo" \
+curl -fsSL "$REPO_URL/ruby3-0-7/client-setup/ruby-build.repo" \\
     -o "/etc/yum.repos.d/ruby-build-3-0-7.repo"
 
 # Refresh cache
